7838d32b94b38e0a5a8c618334205bfb688f7261
freewalker1985 <freewalker1985@126.com>
2018-12-19 19:13:02 +0800

    add code

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -33,11 +33,16 @@ class PostListView(ListView):
 
 def post_share(request, post_id):
     post = get_object_or_404(Post, id=post_id, status='published')
+    sent = False
     if request.method == 'POST':
         form = EmailPostForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
-
+            post_url =  request.build_absolute_uri(post.get_absolute_url())
+            subject = '{} ({}) recommends your reading "{}"'.format(cd['name'], cd['email'], post.title)
+            message = 'Read "{}" at {}\n\n{}\'s comments:{}'.format(post.title, post_url, cd['name'], cd['comments'])
+            send_mail(subject, message, 'freewalker1985@163.com', [cd['to']])
+            sent = True
     else:
         form = EmailPostForm()
-    return render(request, 'blog/post/share.html', {'post': post, 'forms': form})
+    return render(request, 'blog/post/share.html', {'post': post, 'forms': form, 'sent': sent})
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -119,3 +119,10 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+EMIAL_HOST = 'smtp.163.com'
+EMAIL_HOST_USER = 'freewalker1985@163.com'
+EMAIL_HOST_PASSWORD = 'F251285'
+EMAIL_PORT = 465
+EMAIL_USE_SSL = True
