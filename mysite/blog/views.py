3c4a783d75da373db4ef1780eacd6b93a0195a8e
freewalker1985 <freewalker1985@126.com>
2018-12-27 18:21:49 +0800

    add

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,21 +1,31 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.core.mail import send_mail
+from django.db.models import Count
 from django.views.generic import ListView
 from .models import Post, Comment
 
 from .forms import EmailPostForm, CommentForm
+from taggit.models import Tag
 
-# def post_list(request):
-#     object_list = Post.published.all()
-#     paginator = Paginator(object_list, 3)
-#     page = paginator.GET.get('page')
-#     try:
-#         posts = paginator.page(page)
-#     except PageNotAnInteger:
-#         posts = paginator.page(1)
-#     except EmptyPage:
-#         posts = paginator.page(paginator.num_pages)
-#     return render(request, 'blog/post/list.html', {"page": page, 'posts': posts})
+
+def post_list(request, tag_slug=None):
+    object_list = Post.published.all()
+    tag = None
+
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tag__in=[tag])
+
+    paginator = Paginator(object_list, 3)
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    return render(request, 'blog/post/list.html', {"page": page, 'posts': posts, 'tag': tag})
 
 
 def post_detail(request, year, month, day, post):
@@ -32,14 +42,19 @@ def post_detail(request, year, month, day, post):
             new_comment.save()
     else:
         comment_form = CommentForm()
-    return render(request, 'blog/post/detail.html', {'post': post, 'comment_form': comment_form, 'new_comment': new_comment})
+
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_tags = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_tags.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:4]
+    return render(request, 'blog/post/detail.html',
+                  {'post': post, 'comment_form': comment_form, 'new_comment': new_comment, 'similar_posts': similar_posts})
 
 
-class PostListView(ListView):
-    queryset = Post.published.all()
-    context_object_name = 'posts'
-    paginate_by = 3
-    template_name = 'blog/post/list.html'
+# class PostListView(ListView):
+#     queryset = Post.published.all()
+#     context_object_name = 'posts'
+#     paginate_by = 3
+#     template_name = 'blog/post/list.html'
 
 
 def post_share(request, post_id):
@@ -49,7 +64,7 @@ def post_share(request, post_id):
         form = EmailPostForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
-            post_url =  request.build_absolute_uri(post.get_absolute_url())
+            post_url = request.build_absolute_uri(post.get_absolute_url())
             subject = '{} ({}) recommends your reading "{}"'.format(cd['name'], cd['email'], post.title)
             message = 'Read "{}" at {}\n\n{}\'s comments:{}'.format(post.title, post_url, cd['name'], cd['comments'])
             send_mail(subject, message, 'freewalker1985@163.com', [cd['to']])
@@ -57,4 +72,3 @@ def post_share(request, post_id):
     else:
         form = EmailPostForm()
     return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})
-
